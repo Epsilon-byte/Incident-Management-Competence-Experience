51640449a474326e59b2c4a70e764bf1101e14d9
Michaela <radostom@roehampton.ac.uk>
2026-03-11 12:15:45 +0000

    ..

--- Michaela-docker/docker/start-soc.sh
+++ Michaela-docker/docker/start-soc.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+cd "$(dirname "$0")"
+
+if ! docker network inspect hive-network >/dev/null 2>&1; then
+  echo "Creating external Docker network: hive-network"
+  docker network create hive-network
+fi
 
 echo "Starting TheHive..."
 docker compose -f prod1-thehive/docker-compose.yml up -d
